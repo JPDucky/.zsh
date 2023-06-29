c3f5fdfbcd7e18e454a68f0376036818fa6cf8ff
jpducky <34105363+JPDucky@users.noreply.github.com~>
2023-06-29 15:36:46 -0500

    added init script

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+# set the path to the zshrc file
+ZSHRC="$HOME/.zshrc"
+touch .jpducky
+
+# line to check if exists in zshrc
+CONFIG_LINE='for config ($HOME/.zsh/.zshrc) source $config'
+
+if [[ -f .jpducky ]]; then
+    exit 0
+else
+    if [[ -f "$ZSHRC" ]]; then
+        if grep -Fxq "$CONFIG_LINE" "$ZSHRC"; then
+            exit 0
+        else
+            cp "$ZSHRC" $HOME/.zsh/.zshrc
+            sed -i 's/^/#/' $ZSHRC
+            sed -i '1s/^/'"$CONFIG_LINE"'\n/' $ZSHRC
+            exit 0
+        fi
+else
+    echo "$ZSHRC does not exist. Do you have ZSH installed?"
+    echo "Creating file..." 
+    touch $ZSHRC && chmod 755 $ZSHRC
+    echo "Adding config to $ZSHRC"
+    echo "$CONFIG_LINE" >> $ZSHRC
+    exit 0
+fi
+
